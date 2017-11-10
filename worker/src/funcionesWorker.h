40e0fa16a04685c7a2532b204f281d7a649555a6
LarryJVargas <vargas.larryj@gmail.com>
2017-11-10 11:07:59 -0300

    worker: multiples conexiones de desde master

--- worker/src/funcionesWorker.h
+++ worker/src/funcionesWorker.h
@@ -2,6 +2,11 @@
 #define FUNCIONESWORKER_H_
 
 #include <utils.h>
+#include <commons/collections/list.h>
+#include <commons/txt.h>
+//#include <linux/sort.h>
+
+
 
 /* Variables globales */
 t_log* workerLogger;
@@ -10,13 +15,86 @@ char* IP_FILESYSTEM;
 int PUERTO_FILESYSTEM;
 char* ID_NODO;
 int PUERTO_WORKER;
+int PUERTO_DATANODE;
 char* RUTA_DATABIN;
+FILE *filePointer;
+int fileDescriptor;
+char* pathTemporales = "/home/utnso/temp/";
+struct sockaddr_in direccionWorker;
+int socketWorker;
 
+#define UN_MEGABYTE 1048576
+#define UN_BLOQUE sizeof(char)*UN_MEGABYTE
+
+typedef char* t_regArch ;
+
+typedef struct {
+	char* ip;
+	uint32_t puerto;
+	char* nombre;
+}__attribute__((packed)) t_infoArchivo;
+
+typedef struct {
+	uint32_t tamanio;
+	char* nombre;
+	char* contenido;
+} __attribute__((packed)) t_archivoARGlobal;
+
+typedef struct {
+	uint32_t numBloque;
+	uint32_t bytesOcupados;
+	uint32_t largoNombreArchTemp;
+	char* nombreArchTemp;
+	uint32_t largoArchTransformador;
+	char* archTransformador;
+}__attribute__((packed)) t_infoTransformacion;
+
+typedef struct{
+		uint32_t etapa;
+		uint32_t largoRutaArchivoReductorLocal;
+		char* rutaArchivoReductorLocal;
+		uint32_t largoArchivoReductor;
+		char* archivoReductor;
+		uint32_t cantidadTransformaciones;
+		t_list* temporalesTranformacion;
+	}t_infoReduccionLocal;
+
+	typedef struct{
+			uint32_t etapa;
+			uint32_t largoRutaArchivoTemporal;
+			char* rutaArchivoTemporal;
+			uint32_t largoArchivoReductor;
+			char* archivoReductor;
+			uint32_t cantidadNodos;
+			t_list* nodosAConectar;
+		}t_infoReduccionGlobal;
+
+//enums
+#define TRANSFORMACION 10
+#define TRANSFORMACION_OK 11
+#define REDUCCION_LOCAL 14
+#define REDUCCION_LOCAL_OK 15
+#define REDUCCION_GLOBAL 18
+#define REDUCCION_GLOBAL_OK 19
+#define GUARDAR_FINAL 23
+#define LARGO_MAX_LINEA 1048576
 /* Firmas de funciones */
 void crearLogger();
 void cargarArchivoConfiguracion(char*);
 int recibirArchivo(int cliente);
 int recibirYDeserializar(int fd, t_archivo* miArchivo);
 t_archivo* deserializarArchivo(void *buffer);
+void recibirInfoOperacion(int);
+t_infoTransformacion deserializarInfoTransformacion(void* buffer);
+t_infoReduccionLocal deserializarInfoReduccionLocal(void* buffer);
+t_infoReduccionGlobal deserializarInfoReduccionGlobal(void* buffer);
+char* getBloque(int numero);
+void abrirDatabin();
+void cerrarDatabin();
+void realizarTransformacion(t_infoTransformacion);
+void realizarReduccionLocal(t_infoReduccionLocal);
+void realizarReduccionGLobal(t_infoReduccionGlobal);
+char* armarNombreConPathTemp(char*);
+char *guardarArchScript(char*);
 
 #endif
--- worker/src/worker.c
+++ worker/src/worker.c
@@ -5,58 +5,126 @@ int main(void) {
 	NODOARCHCONFIG = "nodoConfig.cfg";
 	cargarArchivoConfiguracion(NODOARCHCONFIG);
 
-	struct sockaddr_in dir;
+	abrirDatabin();
 
-	dir.sin_family = AF_INET;
-	dir.sin_port = htons(24000);
-	dir.sin_addr.s_addr = INADDR_ANY;
-	memset(&(dir.sin_zero), '\0', 8);
+	//struct sockaddr_in direccionDelServidorKernel;
+	direccionWorker.sin_family = AF_INET;
+	direccionWorker.sin_port = htons(PUERTO);
+	direccionWorker.sin_addr.s_addr = INADDR_ANY;
+	//memset(&(direccionYama.sin_zero), '\0', 8);  // Se setea el resto del array de addr_in en 0
 
-	int cliente;
+	int activado = 1;
+	int pid;
 
-	cliente = socket(AF_INET, SOCK_STREAM, 0);
-
-	// Olvidémonos del error "Address already in use" [La dirección ya se está usando]
-	/*
-	 int yes=1;
-	 if (setsockopt(ssocket,SOL_SOCKET,SO_REUSEADDR,&yes,sizeof(int)) == -1) {
-	 perror("setsockopt");
-	 exit(1);
-	 }
-	 */
-
-	if (connect(cliente, (struct sockaddr *) &dir, sizeof(struct sockaddr)) != 0) {
-		perror("fallo el connect");
-		return 1;
-	}
-	/*
-	 struct sockaddr_in direccionCliente;
-	 int tamanioDir = sizeof(struct sockaddr_in);
-	 int cliente = accept(ssocket, (void*)&direccionCliente, &tamanioDir);
-	 printf("Entro una conexion por el puerto %d\n", cliente);
-
-	 send(cliente, "Hola mundo\n", 11, 0);
-	 */
-	/**********************************************************/
-
-	int bytesRecibidos = recibirArchivo(cliente);
-	if (bytesRecibidos < 0) {
-		perror("Error");
-		return 1;
+	socketWorker = socket(AF_INET, SOCK_STREAM, 0);
+	// Permite reutilizar el socket sin que se bloquee por 2 minutos
+	if (setsockopt(socketWorker, SOL_SOCKET, SO_REUSEADDR, &activado,
+			sizeof(activado)) == -1) {
+		perror("setsockopt");
+		exit(1);
 	}
 
+	// Se enlaza el socket al puerto
+	if (bind(socketWorker, (struct sockaddr *) &direccionWorker,
+			sizeof(struct sockaddr)) != 0) {
+		perror("No se pudo conectar");
+		exit(1);
+	}
+	// Se pone a escuchar el servidor kernel
+	if (listen(socketWorker, 10) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	fd_set readfds, auxRead;
+	int tamanioDir = sizeof(direccionWorker);
+	char* buffer;
+	int bytesRecibidos, maxPuerto, i, nuevoSocket;
+	FD_ZERO(&readfds);
+	FD_ZERO(&auxRead);
+	FD_SET(socketWorker, &auxRead);
+
+	maxPuerto = socketWorker;
+
+	printf("escuchando masters\n");
 	while (1) {
-		char mensaje[100];
-		scanf("%s", mensaje);
 
-		send(cliente, mensaje, strlen(mensaje), 0);
-		/*
-		 bytesRecibidos = recibirArchivo(bytesRecibidos, cliente, len, fich);
-		 if(bytesRecibidos < 0){
-		 perror("Error");
-		 return 1;
-		 }
-		 */
+		readfds = auxRead;
+		if (select(maxPuerto + 1, &readfds, NULL, NULL, NULL) == -1) {
+			perror("select");
+			exit(1);
+		}
+
+		for (i = 0; i <= maxPuerto; i++) {
+			if (FD_ISSET(i, &readfds)) {
+				if (i == socketWorker) {
+
+					if ((nuevoSocket = accept(socketWorker,
+							(void*) &direccionWorker, &tamanioDir)) <= 0)
+						perror("accept");
+					else {
+						//Le envia el archivo apenas se conecta con un puerto
+						printf("Entro una conexion por el puerto %d\n",
+								nuevoSocket);
+						FD_SET(nuevoSocket, &auxRead);
+
+						void* buffer;
+						t_infoTransformacion infoTransformacion;
+						t_infoReduccionLocal infoReduccionLocal;
+						t_infoReduccionGlobal infoReduccionGlobal;
+						t_header header;
+						recibirHeader(nuevoSocket, &header);
+						buffer = malloc(header.tamanioPayload);
+						recibirPorSocket(nuevoSocket, buffer,header.tamanioPayload);
+						pid=fork();
+						switch (header.id) {
+
+						case TRANSFORMACION:
+							infoTransformacion=deserializarInfoTransformacion(buffer);
+							realizarTransformacion(infoTransformacion);
+							break;
+						case REDUCCION_LOCAL:
+							infoReduccionLocal=deserializarInfoReduccionLocal(buffer);
+							realizarReduccionLocal(infoReduccionLocal);
+							break;
+						case REDUCCION_GLOBAL:
+							infoReduccionGlobal=deserializarInfoReduccionGlobal(buffer);
+							realizarReduccionGLobal(infoReduccionGlobal);
+							break;
+						}
+						waitpid();
+							//t_rutaArchivo* ruta;
+							//ruta = malloc(sizeof(t_rutaArchivo));
+
+							//recibirRutaDeArchivoAProcesar(nuevoSocket,&ruta);
+							//recive la ruta del archivo
+							if (nuevoSocket > maxPuerto)
+								maxPuerto = nuevoSocket;
+						}
+					}
+				else {
+					buffer = malloc(1000);
+
+					bytesRecibidos = recibirPorSocket(i, buffer, 1000);
+					if (bytesRecibidos < 0) {
+						perror("Error");
+						free(buffer);
+						exit(1);
+					}
+					if (bytesRecibidos == 0) {
+						//printf("Se desconecto del fileSystem el socket %d", i);
+						FD_CLR(i, &readfds);
+						shutdown(i, 2);
+						free(buffer);
+					} else {
+						buffer[bytesRecibidos] = '\0';
+						printf(
+								"Socket: %d -- BytesRecibidos: %d -- Buffer recibido : %s\n",
+								i, bytesRecibidos, buffer);
+
+					}
+				}
+			}
+		}
 	}
-	return EXIT_SUCCESS;
 }
